2905e16e126ced1a17eb258682be5df82ecb71f0
Ben Robertson <35968223+bwrobertson@users.noreply.github.com>
2018-09-19 15:23:28 -0600

    Update bootlegShell.py
    Added the piping method

--- bootlegShell.py
+++ bootlegShell.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import os,sys,argparse,re
 
@@ -8,16 +8,9 @@ from contextlib import redirect_stdout
 class bootlegShell(Cmd):
 
 
-
-	def do_test(self, args):
-		"""The shell itself, follow the prompts on screen. Output file must be created beforehand."""
+	def pipeMethod(self):
 		pid = os.getpid()
-		print("Input file name...or press enter")
-		fileName = input()
-		print("Input command...")
-		commandName = input()
-		print("Input output file name... or press enter")
-		outFile = input()
+		global pipeCommand
 		os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
 		rc = os.fork()
 
@@ -28,7 +21,60 @@ class bootlegShell(Cmd):
 		elif rc == 0:                   # child
 		    #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
 		    #             (os.getpid(), pid)).encode())
-			args = [commandName]
+			args = [pipeCommand]
+			for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+				program = "%s/%s" % (dir, args[0])
+		        #os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+				try:
+					normalIn = sys.stdin
+					sys.stdin = open("pipe1.txt", "r")
+					os.dup2(sys.stdin.fileno(), 0)
+					os.execve(program, args, os.environ) # try to exec program
+					sys.stdin = normalIn
+				except FileNotFoundError:             # ...expected
+					pass                              # ...fail quietly
+			os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
+			sys.exit(1)                 # terminate with error
+
+		else:                           # parent (forked ok)
+		    #os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
+		    #             (pid, rc)).encode())
+		    childPidCode = os.wait()
+		    os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
+		                 childPidCode).encode())                          # ...fail quietly
+
+	def do_CDR(self, args):
+		"""The shell itself, follow the prompts on screen. Output file must be created beforehand."""
+		pid = os.getpid()
+		print("Input command...")
+		commandName = input()
+		commands = commandName.split()
+		command = commands[0]
+		fileName=""
+		pipe=False
+		outFile=""
+		global pipeCommand
+		global program
+		for x in range(len(commands)):
+			if(commands[x]=='<'):
+				fileName=commands[x+1]
+			if(commands[x]=='>'):
+				outFile=commands[x+1]
+			if(commands[x]=='|'):
+				outFile="pipe1.txt"
+				pipe=True
+				pipeCommand=commands[x+1]
+		os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
+		rc = os.fork()
+
+		if rc < 0:
+		    os.write(2, ("fork failed, returning %d\n" % rc).encode())
+		    sys.exit(1)
+
+		elif rc == 0:                   # child
+		    #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+		    #             (os.getpid(), pid)).encode())
+			args = [command]
 			for dir in re.split(":", os.environ['PATH']): # try each directory in the path
 				program = "%s/%s" % (dir, args[0])
 		        #os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
@@ -39,6 +85,8 @@ class bootlegShell(Cmd):
 						sys.stdin = open(fileName,"r")
 					if(outFile!=""):
 						sys.stdout = open(outFile,"w")
+					if(pipe):
+						sys.stdout = open("pipe1.txt", "w")
 					os.dup2(sys.stdin.fileno(), 0)
 					os.dup2(sys.stdout.fileno(), 1)
 					os.execve(program, args, os.environ) # try to exec program
@@ -55,6 +103,8 @@ class bootlegShell(Cmd):
 		    childPidCode = os.wait()
 		    os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
 		                 childPidCode).encode())
+		if(pipe==True):
+			shell.pipeMethod()
 
 
 	def do_hello(self, args):
@@ -71,5 +121,3 @@ if __name__ == '__main__':
     shell = bootlegShell()
     shell.shell = '> '
     shell.cmdloop('Starting prompt...')
-
-#The do_hello and do_quit methods were based on the ones at https://coderwall.com/p/w78iva/give-your-python-program-a-shell-with-the-cmd-module
